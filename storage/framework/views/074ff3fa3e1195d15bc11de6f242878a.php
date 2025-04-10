8fa7db195ad2070f0d005575eaa49880d0803c74
Dave <davealmadin3196@gmail.com>
2025-04-10 19:02:20 +0800

    default env file

--- storage/framework/views/074ff3fa3e1195d15bc11de6f242878a.php
+++ storage/framework/views/074ff3fa3e1195d15bc11de6f242878a.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        
-        <title>Bridge Hope Payment System</title>
+        <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+      
         
 
         <!-- Fonts -->
