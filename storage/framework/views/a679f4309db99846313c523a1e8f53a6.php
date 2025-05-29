f1693273d2096401288d506d9cef4c93b9244161
Dave <davealmadin3196@gmail.com>
2025-05-29 14:09:33 +0800

    Footer Edited

--- storage/framework/views/a679f4309db99846313c523a1e8f53a6.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-    <h1>Hello World!</h1>
-    <p><?php echo e($Message); ?></p>
-</div>
-<?php /**PATH C:\xampp\htdocs\dashboard\payment-system\resources\views/WelcomeEmail.blade.php ENDPATH**/ ?>
