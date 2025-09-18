cf19f80799f8ddfd7e1ad07925a764bcb8ac6a4f
Dave <davealmadin3196@gmail.com>
2025-09-18 21:37:50 +0800

    Welcome page still in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Payment Management</title>
+    <title>LCMS</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -906,8 +906,8 @@
     @endif
 </head>
 
-<body class="font-sans antialiased bg-black dark:text-white/50 overflow-x-hidden">
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 w-full h-full bg-no-repeat bg-cover bg-center"
+<body class="font-sans antialiased bg-gray-100 h-screen overflow-x-hidden">
+    {{-- <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 w-full h-full bg-no-repeat bg-cover bg-center"
         style="background-image:url('https://cdn.pixabay.com/photo/2022/07/31/06/09/day-care-center-7355023_1280.jpg');">
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
@@ -915,7 +915,7 @@
                 <header class="grid items-center gap-2 py-10 lg:grid-cols-3">
                     <div class="flex lg:justify-center lg:col-start-2">
                         @if (Route::has('login'))
-                            <nav class="-mx-3 flex flex-1 justify-center">
+                            <nav class="-mx-3 flex flex-1 justify-center gap-2">
                                 @auth
                                     <a href="{{ url('/dashboard') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white bg-black/50">
@@ -927,34 +927,39 @@
                                         Log in
                                     </a>
 
-                                    {{-- @if (Route::has('register'))
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white bg-black/50">
                                             Register
                                         </a>
-                                    @endif --}}
+                                    @endif
                                 @endauth
                             </nav>
                         @endif
                     </div>
-
                 </header>
-
-                {{-- <footer class="py-16 text-center text-sm text-black dark:text-white/70 w-full bg-black">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})<br>
-                        Powered by: JDV Developer<br>
-                        Copyright 2025 All rights reserved
-                    </footer> --}}
             </div>
         </div>
-        <footer class="py-16 text-center text-sm text-black dark:text-white/70 w-full bg-black">
-           <p class="text-base">Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p> 
+        <footer class="py-6 text-center text-sm text-black dark:text-white/70 w-full bg-black">
+            <p class="text-base">Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+            </p>
             <p class="text-base">Powered by: JDV Developer</p>
             <p class="text-base">Copyright 2025 All rights reserved</p>
         </footer>
+    </div> --}}
+    <div class="grid grid-cols-3 gap-4 p-10">
+        <div class="col-span-3 rounded-lg md:col-span-1 bg-gray-500 h-50">
+            <div class="flex items-center justify-center">
+                <p class="">Left Column</p>
+            </div>
+        </div>
+        <div class="col-span-3 rounded-lg md:col-span-2 bg-yellow-500">
+            <div class="flex items-center justify-center">
+                <p class="">Left Column</p>
+            </div>
+        </div>
     </div>
+
 </body>
 
 </html>
